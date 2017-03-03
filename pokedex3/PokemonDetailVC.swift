117d6a76531d001f0bd009efbb08445247565550
imdaeshawn <imdaeshawn@gmail.com>
2017-03-03 08:51:32 -0500

    created vars for detail view and added the outlets.

--- pokedex3/PokemonDetailVC.swift
+++ pokedex3/PokemonDetailVC.swift
@@ -12,7 +12,21 @@ class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     
+    
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currenteEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +35,9 @@ class PokemonDetailVC: UIViewController {
         
     }
 
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        
+      dismiss(animated: true, completion: nil)
+    }
     
-
 }
